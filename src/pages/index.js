311917f3d16f756729648e2196bfb04bfd5cb54e
habiburrahman0540 <habiburrahman0540@gmail.com>
2024-06-09 11:39:19 +0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import HomePage from './HomePage'
+import AboutPage from './AboutPage'
+import CartPage from './CartPage'
+import CheckoutPage from './CheckoutPage'
+import ErrorPage from './ErrorPage'
+import PrivateRoute from './PrivateRoute'
+import ProductsPage from './ProductsPage'
+import SingleProductPage from './SingleProductPage'
+
+export {
+    HomePage,
+    AboutPage,
+    CartPage,
+    CheckoutPage,
+    ErrorPage,
+    PrivateRoute,
+    ProductsPage,
+    SingleProductPage
+};
